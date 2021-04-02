dfa5ee5f22d94a3cecf205ad88bc640e7b374391
0354 <0354@DESKTOP-MKOV7LM>
2021-04-02 22:39:37 +0530

    Sauce Lab run updated

--- WalletHub-Assignments/src/test/java/WalletHubTest/FacebookLoginTest.java
+++ WalletHub-Assignments/src/test/java/WalletHubTest/FacebookLoginTest.java
@@ -1,10 +1,14 @@
 package WalletHubTest;
 
 import java.awt.AWTException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -21,15 +25,26 @@ public class FacebookLoginTest {
 	String url =  "https://www.facebook.com/";
 	String username = "Test";
 	String password = "Test";
+	 public static final String USERNAME = "BlackWolf89";
+	 public static final String ACCESS_KEY = "d6e226ef-2d16-4aa9-acf8-4c13abee8033";
+	 public static final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub";
 	
 	@BeforeTest
-	public void setChromeDriver() {
+	public void setChromeDriver() throws MalformedURLException {
 		//To disable browser notifications
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--disable-notifications");
+//		ChromeOptions options = new ChromeOptions();
+//		options.addArguments("--disable-notifications");
+//		
+//		System.setProperty("webdriver.chrome.driver",".\\src\\main\\resources\\chromedriver.exe");
+//		driver = new ChromeDriver(options);
+		DesiredCapabilities dc = new DesiredCapabilities().chrome();
+		dc.setCapability("platform", "Windows 10");
+		dc.setCapability("version", "98.0");
+		dc.setCapability("build", "build-1.2");
+		dc.setCapability("name", "Git Test");
 		
-		System.setProperty("webdriver.chrome.driver",".\\src\\main\\resources\\chromedriver.exe");
-		driver = new ChromeDriver(options);
+		driver = new RemoteWebDriver(new URL(URL) , dc);
+		
 		fbLoginPO = new FacebookLogInPage(driver);
 		fbHomePO = new FacebookHomePage(driver);
 	}
